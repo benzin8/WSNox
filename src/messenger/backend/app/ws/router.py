f3aa83019b58292ac4fda31322a66cc10a9eba30
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2026-02-17 17:46:26 +0300

    feat(ws): updated websocket router

--- src/messenger/backend/app/ws/router.py
+++ src/messenger/backend/app/ws/router.py
@@ -1,32 +1,55 @@
 from fastapi import WebSocket, APIRouter
 from fastapi.websockets import WebSocketDisconnect
-from typing import List
+from typing import Dict
 
 class ConnectionManager:
     def __init__(self) -> None:
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: Dict[int, WebSocket] = {}
 
-    async def connect(self, websocket: WebSocket) -> None:
+    async def connect(self, websocket: WebSocket, user_id: int) -> None:
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket
 
-    def disconnect(self, websocket: WebSocket) -> None:
-        self.active_connections.remove(websocket)
+    def disconnect(self, user_id: int) -> None:
+        if user_id in self.active_connections:
+            del self.active_connections[user_id]
 
-    async def send_message(self, message: str) -> None:
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def send_message(self, message: str, sender_id: int) -> None:
+        for uid, connection in self.active_connections.items():
+            if uid != sender_id:
+                await connection.send_text(message)
+    
+    async def send_personal_message(self,
+                                    message: str,
+                                    recipient_id: int) -> None:
+        if recipient_id in self.active_connections:
+            try:
+                await self.active_connections[recipient_id].send_text(message)
+            except Exception:
+                del self.active_connections[recipient_id]
+        else:
+            print(f"Пользователь {recipient_id} не в сети!")
 
 manager = ConnectionManager()
 
 ws_router = APIRouter()
 
-@ws_router.websocket("/chat")
-async def websocket_chat(websocket: WebSocket) -> None:
-    await manager.connect(websocket)
+@ws_router.websocket("/chat/{user_id}")
+async def websocket_chat(websocket: WebSocket, user_id: int) -> None:
+    await manager.connect(websocket, user_id)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_message(data)
+
+            import json
+            msg_data = json.loads(data)
+            recipient_id = msg_data.get('recipient_id')
+            text = msg_data["message"]
+
+            await manager.send_personal_message(
+                text,
+                recipient_id
+            )
+
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        manager.disconnect(int(user_id))
