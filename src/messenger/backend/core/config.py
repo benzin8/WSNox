8e7b8b6ad5d124893e484e75957bb44ca0c59b5f
Дмитрий Вислобоков <visdima0102@gmail.com>
2026-03-11 22:46:01 +0300

    add: Settings class

---
+++ src/messenger/backend/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    db_user: str
+    db_pass: str
+    db_host: str
+    db_port: int
+    db_name: str
+    
+    secret_key: str
+    algorithm: str
+
+    @property
+    def database_url(self) -> str:
+        return f"mysql+aiomysql://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
