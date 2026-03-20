6380a3d98bd83bf0efc1f4e39471b94d8cb1c826
Дмитрий Вислобоков <visdima0102@gmail.com>
2026-03-20 18:22:24 +0300

    update: phone number is required upon registration

--- src/messenger/backend/models/user.py
+++ src/messenger/backend/models/user.py
@@ -13,8 +13,9 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    phone_number: Mapped[Optional[str]] = mapped_column(String(20), unique=True, nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     email: Mapped[Optional[str]] = mapped_column(String(255), unique=True, nullable=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
