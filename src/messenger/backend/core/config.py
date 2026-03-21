751ed892dd868fcae319f564af35203cbb44d6a0
Дмитрий Вислобоков <visdima0102@gmail.com>
2026-03-22 01:02:54 +0300

    feat: comfortable start project without change .env file

--- src/messenger/backend/core/config.py
+++ src/messenger/backend/core/config.py
@@ -1,3 +1,4 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -10,6 +11,21 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     redis_url: str
+
+    DOCKER_MODE: bool = os.getenv("DOCKER_MODE", "false").lower() == "true"
+
+    @property
+    def db_host(self) -> str:
+        if self.DOCKER_MODE:
+            return "db" 
+        return "127.0.0.1" 
+
+    @property
+    def redis_host(self) -> str:
+        if self.DOCKER_MODE:
+            return "redis" 
+        return "127.0.0.1" 
+
     @property
     def database_url(self) -> str:
         return f"mysql+aiomysql://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
