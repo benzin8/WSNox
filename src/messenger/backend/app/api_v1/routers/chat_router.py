4c26504f5bc695757c1d41e50dab98cdc7400226
benzin8 <visdima0102@gmail.com>
2026-04-06 13:47:14 +0300

    refactor: edit return data to /get-or-create endpoint, and add endpoint to get user data like name and username

--- src/messenger/backend/app/api_v1/routers/chat_router.py
+++ src/messenger/backend/app/api_v1/routers/chat_router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from messenger.backend.core.redis import get_redis
 from messenger.backend.db.session import get_db_session
 from messenger.backend.app.api_v1.auth.dependencies import get_current_user, oauth2_scheme
 
 from messenger.backend.app.api_v1.schemas.chat import UserSearchResponse, ChatCreateRequest, ChatResponse
+from messenger.backend.app.api_v1.schemas.user import UserResponse
 
 from messenger.backend.app.crud.chat import ChatCRUD
 
@@ -20,11 +20,17 @@ async def search_users(query: str, db: AsyncSession = Depends(get_db_session), u
 async def get_or_create_chat(request: ChatCreateRequest, db: AsyncSession = Depends(get_db_session), current_user=Depends(get_current_user), _token: str = Depends(oauth2_scheme)):
     exising_chat = await ChatCRUD.get_chat_by_user_id(db, current_user.id, request.other_user_id)
     if exising_chat:
-        return ChatResponse(chat=exising_chat)
+        return ChatResponse.model_validate(exising_chat)
     
     new_chat = await ChatCRUD.create_private_chat(
         session=db,
         chat_data=request,
-        members=[current_user.id, request.other_user_id]
+        members=[current_user.id, request.other_user_id],
+        current_user=current_user
     )
-    return ChatResponse(chat=new_chat)
+    return ChatResponse.model_validate(new_chat)
+
+@chat_router.get("/{chat_id}/user", response_model=UserResponse)
+async def get_user_data_by_chat_id(chat_id: int, db: AsyncSession = Depends(get_db_session), current_user=Depends(get_current_user), _token: str = Depends(oauth2_scheme)):
+    user = await ChatCRUD.get_user_data_by_chat_id(db, chat_id, current_user.id)
+    return UserResponse.model_validate(user)
