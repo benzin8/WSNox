4e341921c3106562d06a430953a72a2dd4f01ceb
benzin8 <visdima0102@gmail.com>
2026-04-06 13:44:12 +0300

    feat: upgrade ChatCRUD and add chat_type to ChatResponse schema

--- src/messenger/backend/app/api_v1/schemas/chat.py
+++ src/messenger/backend/app/api_v1/schemas/chat.py
@@ -8,8 +8,9 @@ from messenger.backend.app.api_v1.schemas.user import UserResponse
 class ChatResponse(BaseModel):
     id: int
     name: str
-    last_message: Optional[str]
-    last_message_time: Optional[datetime]
+    chat_type: str
+    last_message: Optional[str] = None
+    last_message_time: Optional[datetime] = None
     
     class Config:
         from_attributes = True
@@ -18,6 +19,5 @@ class UserSearchResponse(BaseModel):
     chats: List[UserResponse]
     
 class ChatCreateRequest(BaseModel):
-    user_id: int
     other_user_id: int
 
