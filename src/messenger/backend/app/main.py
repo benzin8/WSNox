c58146784d1c0a245f613af92722db88580141eb
benzin8 <visdima0102@gmail.com>
2026-04-06 13:45:00 +0300

    refactor: add backend origins

--- src/messenger/backend/app/main.py
+++ src/messenger/backend/app/main.py
@@ -30,6 +30,8 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",    # Стандартный порт Vite
     "http://127.0.0.1:5173",
+    "http://localhost:8000",    # Docker backend
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
