c9f46f47099b2020e89b6fb4b0bdedef3a12f6eb
benzin8 <visdima0102@gmail.com>
2026-04-13 22:58:00 +0300

    feat: add message CRUD

---
+++ src/messenger/backend/app/crud/message.py
@@ -0,0 +1,31 @@
+from messenger.backend.db.models import Message
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from messenger.backend.core.crypto import encrypt_message, decrypt_message
+
+class MessageCRUD:
+    @staticmethod
+    async def create_text_message(db: AsyncSession, chat_id: int, sender_id: int, recipient_id: int, text: str) -> Message:
+        encrypted_text = encrypt_message(text)
+        message = Message(
+            chat_id = chat_id,
+            sender_id = sender_id,
+            recipient_id = recipient_id,
+            encrypted_data = encrypted_text
+        )
+        db.add(message)
+        await db.commit()
+        await db.refresh(message)
+        return message
+
+    @staticmethod
+    async def get_messages(db: AsyncSession, chat_id: int) -> list[Message]:
+        query = (
+            select(Message)
+            .where(Message.chat_id == chat_id)
+            .order_by(Message.created_at.desc())
+            .limit(100)
+        ) # Вывод последних 100 сообщений
+        result = await db.execute(query)
+        return result.scalars().all()
