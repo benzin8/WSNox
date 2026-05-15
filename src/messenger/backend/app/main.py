c56ce6d9c4a34d28ac30630cbc00ede420a7a152
benzin <benzin8enzi@gmail.com>
2026-05-16 02:09:29 +0300

    feat(profiles): register profile router in app

--- src/messenger/backend/app/main.py
+++ src/messenger/backend/app/main.py
@@ -6,6 +6,7 @@ from messenger import PROJECT_ROOT
 from .api_v1.routers import frontend_router
 from .api_v1.routers.auth_router import auth_router
 from .api_v1.routers.chat_router import chat_router
+from .api_v1.routers.profile_router import profile_router
 from .ws.router import ws_router
 from messenger.backend.core.redis import init_redis, close_redis
 
@@ -59,4 +60,5 @@ app.mount("/assets",
 app.include_router(ws_router)
 app.include_router(auth_router)
 app.include_router(chat_router)
+app.include_router(profile_router)
 app.include_router(frontend_router)
