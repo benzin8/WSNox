4cd625383498754229bd63f5f37a88563fb4dccb
benzin8 <visdima0102@gmail.com>
2026-05-18 14:39:44 +0300

    feat: update auth router to use email instead of phone number

--- src/messenger/backend/app/api_v1/routers/auth_router.py
+++ src/messenger/backend/app/api_v1/routers/auth_router.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from messenger.backend.app.api_v1.schemas.user import (
     AuthResponse,
-    PhoneNumberRequest,
-    PhoneVerify,
+    EmailRequest,
+    EmailVerify,
     UserCreate,
     UserLogin,
     UserResponse,
@@ -17,47 +17,48 @@ from messenger.backend.services.verification import send_verification_code, veri
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
+
 @auth_router.post("/send-code")
-async def send_code(data: PhoneNumberRequest):
-    await send_verification_code(data.phone_number)
+async def send_code(data: EmailRequest):
+    await send_verification_code(data.email)
     return {"message": True}
 
+
 @auth_router.post("/verify-code")
-async def verify_sms(data: PhoneVerify, db: AsyncSession = Depends(get_db_session)):
-    is_valid = await verify_code(data.phone_number, data.code)
+async def verify_sms(data: EmailVerify, db: AsyncSession = Depends(get_db_session)):
+    is_valid = await verify_code(data.email, data.code)
     if not is_valid:
         raise HTTPException(status_code=400, detail="Invalid code")
-    
-    user = await UserCRUD.get_user_by_phone(db, data.phone_number)
+
+    user = await UserCRUD.get_user_by_email(db, data.email)
     redis = get_redis()
     if user:
-        await redis.setex(f"verifed_for_login:{data.phone_number}", 300, "true")
+        await redis.setex(f"verified_for_login:{data.email}", 300, "true")
         return {
             "status": "need_password",
-            "message": "SMS verified. Please enter your password."
+            "message": "Email verified. Please enter your password."
         }
     else:
-        await redis.setex(f"verifed_for_reg:{data.phone_number}", 600, "true")
+        await redis.setex(f"verified_for_reg:{data.email}", 600, "true")
         return {
             "status": "register",
-            "message": "Phone number verified. Please register."
-            }
-        
- 
+            "message": "Email verified. Please register."
+        }
+
+
 @auth_router.post("/register", response_model=AuthResponse)
 async def register(data: UserCreate, db: AsyncSession = Depends(get_db_session)):
     redis = get_redis()
-
-    verifed_number = await redis.get(f"verifed_for_reg:{data.phone_number}")
-    if not verifed_number:
+    verified = await redis.get(f"verified_for_reg:{data.email}")
+    if not verified:
         raise HTTPException(status_code=400, detail="Phone number not verified")
 
     user = await UserCRUD.create_user(db, data, data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="User with this phone number already exists")
+        raise HTTPException(status_code=400, detail="User with this email already exists")
 
     tokens = create_pair_jwt_tokens(user.id)
-    await redis.delete(f"verifed_for_reg:{data.phone_number}")
+    await redis.delete(f"verified_for_reg:{data.email}")
     return {
         "status": "success",
         "user": UserResponse.model_validate(user),
@@ -65,24 +66,21 @@ async def register(data: UserCreate, db: AsyncSession = Depends(get_db_session))
         "refresh_token": tokens["refresh_token"],
         "token_type": "bearer"
     }
-    
+
+
 @auth_router.post("/login", response_model=AuthResponse)
 async def login(data: UserLogin, db: AsyncSession = Depends(get_db_session)):
     redis = get_redis()
-
-    phone_number = data.phone_number
-    password = data.password
-    
-    verifed_number = await redis.get(f"verifed_for_login:{phone_number}")
-    if not verifed_number:
+    verified = await redis.get(f"verified_for_login:{data.email}")
+    if not verified:
         raise HTTPException(status_code=400, detail="Phone number not verified")
 
-    user = await UserCRUD.login_user(db, phone_number, password)
-    if not user or not verify_password(password, user.hashed_password):
-        raise HTTPException(status_code=400, detail="Invalid phone number or password")
+    user = await UserCRUD.login_user(db, data.email, data.password)
+    if not user or not verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
 
     tokens = create_pair_jwt_tokens(user.id)
-    await redis.delete(f"verifed_for_login:{phone_number}")
+    await redis.delete(f"verified_for_login:{data.email}")
     return {
         "status": "success",
         "user": UserResponse.model_validate(user),
